30dd487b2e5784455c2753a4785bcbec80d24180
Zaid Alshobaki <zaidalshobaki05@gmail.com>
2026-08-19 22:08:33 +0300

    Initialize cognition package

---
+++ app/cognition/__init__.py
@@ -0,0 +1,13 @@
+from .engine import CaseCognitionEngine
+from .models import CaseModel, Fact, Actor, Event, EvidenceItem, LegalHypothesis, ClarifyingQuestion
+
+__all__ = [
+    "CaseCognitionEngine",
+    "CaseModel",
+    "Fact",
+    "Actor",
+    "Event",
+    "EvidenceItem",
+    "LegalHypothesis",
+    "ClarifyingQuestion",
+]
