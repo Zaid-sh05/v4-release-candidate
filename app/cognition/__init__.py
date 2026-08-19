7b4943c70a4724c6aed147f3599fea8a2d1bbbdc
Zaid Alshobaki <zaidalshobaki05@gmail.com>
2026-08-19 22:11:44 +0300

    Export conversation cognition state

--- app/cognition/__init__.py
+++ app/cognition/__init__.py
@@ -1,8 +1,10 @@
 from .engine import CaseCognitionEngine
+from .conversation import ConversationCaseState
 from .models import CaseModel, Fact, Actor, Event, EvidenceItem, LegalHypothesis, ClarifyingQuestion
 
 __all__ = [
     "CaseCognitionEngine",
+    "ConversationCaseState",
     "CaseModel",
     "Fact",
     "Actor",
